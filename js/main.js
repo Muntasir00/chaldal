97a676f6be0aed6901129682ad557c164e6ceb4d
Sagor Chandra Paul <paulsagor1995@gmail.com>
2021-02-15 09:57:19 +0600

    product-details page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-// var opt = {
-//     animation : true
-// };
-// var myTooltipEl = document.getElementById('myTooltip')
-// var tooltip = new bootstrap.Tooltip(myTooltipEl, opt);
-
-// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-//     return new bootstrap.Tooltip(tooltipTriggerEl)
-// })
 
 $(document).ready(function (){
     $('[data-toggle="tooltip"]').tooltip({
